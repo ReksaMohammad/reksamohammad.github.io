eb0cbbc5ca30570a3a9381fde64c9da9883f91ee
Reksa <42407562+ReksaMohammad@users.noreply.github.com>
2021-04-18 03:55:07 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarNav')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+
