9487562de8f3bab3dc313e52ba4d6e8c62920148
Reksa <42407562+ReksaMohammad@users.noreply.github.com>
2021-04-18 19:57:48 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById('navbarSupportedContent').addEventListener('click', function(){
+    document.getElementById('navbarSupportedContent').addEventListener('click', function () {
         var el = document.getElementById('navbarSupportedContent')
         el.classList.remove("show");
-      });
+    });
+
+    var links = document.getElementsByTagName("a");
+
+    //Browse the previously created array
+    Array.prototype.forEach.call(links, function (elem, index) {
+        //Get the hyperlink target and if it refers to an id go inside condition
+        var elemAttr = elem.getAttribute("href");
+        if (elemAttr && elemAttr.includes("#")) {
+            //Replace the regular action with a scrolling to target on click
+            elem.addEventListener("click", function (ev) {
+                ev.preventDefault();
+                //Scroll to the target element using replace() and regex to find the href's target id
+                document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "nearest"
+                });
+            });
+        }
+    });
 });
