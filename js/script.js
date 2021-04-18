caebe5f89d5956994b919590b95f211c2400e9de
Reksa <42407562+ReksaMohammad@users.noreply.github.com>
2021-04-18 19:43:55 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('navbarSupportedContent').addEventListener('click', function(){
+        var el = document.getElementById('navbarSupportedContent')
+        el.classList.remove("show");
+      });
+});
